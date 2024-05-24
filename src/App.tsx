b9eaa4ad9a81527d3ded8a84d7ab9b940d2f1473
rohansthaart <rohan-stha@live.com>
2024-05-24 23:24:58 +0545

    add game

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { StyleSheet, Text, View, Image,  ImageSourcePropType, Pressable } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageSourcePropType, Pressable } from 'react-native';
 import React, { PropsWithChildren } from 'react';
 import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
 
@@ -36,39 +36,42 @@ const Dice = ({ imageUrl }: DiceProps): JSX.Element => {
 
 export default function App(): JSX.Element {
   const [diceImage, setDiceImage] = React.useState<ImageSourcePropType>(DiceOne);
+  const [dice2Image, setDice2Image] = React.useState<ImageSourcePropType>(DiceOne);
+  const diceImages = [DiceOne, DiceTwo, DiceThree, DiceFour, DiceFive, DiceSix];
+  const [result, setResult] = React.useState<string>('');
+
+  const getRandomDice = () => {
+    let randomNumber1 = Math.floor(Math.random() * 6);
+    let randomNumber2 = Math.floor(Math.random() * 6);
+
+    setDiceImage(diceImages[randomNumber1]);
+    setDice2Image(diceImages[randomNumber2]);
+
+    if (randomNumber1 > randomNumber2) {
+      return ('Player 1 wins 🚩');
+    } else if (randomNumber1 < randomNumber2) {
+      return ('Player 2 wins 🚩');
+    } else {
+      return ('Draw 🎌');
+    } 
+  };
 
   const rollDiceOnTap = () => {
-    let randomNumber = Math.floor(Math.random() * 6) + 1;
-    switch (randomNumber) {
-      case 1:
-        setDiceImage(DiceOne);
-        break;
-      case 2:
-        setDiceImage(DiceTwo);
-        break;
-      case 3:
-        setDiceImage(DiceThree);
-        break;
-      case 4:
-        setDiceImage(DiceFour);
-        break;
-      case 5:
-        setDiceImage(DiceFive);
-        break;
-      case 6:
-        setDiceImage(DiceSix);
-        break;
-      default:
-        setDiceImage(DiceOne);
-        break;
-    }
-    ReactNativeHapticFeedback.trigger("impactLight", options);
-  }
+    const result = getRandomDice();
+    ReactNativeHapticFeedback.trigger('impactLight', options);
+    setResult(result);
+  };
 
   return (
 
     <View style={styles.container}>
-      <Dice imageUrl={diceImage} />
+      <Text style={styles.heading}>Roll The Dice</Text>
+      <Text style={styles.result}>{result}</Text>
+      <View style={styles.diceContainer}>
+        <Dice imageUrl={diceImage} />
+        <Dice imageUrl={dice2Image} />
+
+      </View>
       <Pressable onPress={rollDiceOnTap} style={styles.rollDiceBtn}><Text style={styles.rollDiceText}>Roll The Dice</Text></Pressable>
 
     </View>
@@ -83,8 +86,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff2f2',
   },
   diceImage: {
-    width: 200,
-    height: 200,
+    width: 150,
+    height: 150,
     margin: 20,
   },
   heading: {
@@ -101,6 +104,14 @@ const styles = StyleSheet.create({
   rollDiceText: {
     color: 'white',
   },
-
+  diceContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  result: {
+    fontSize: 20,
+    marginBottom: 20,
+    color: 'green',
+  }
 });
 
